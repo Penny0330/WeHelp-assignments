6212e8f56108c9a98154d242c7fbbc22266ea1d6
Penny Lin <ox1566@gmail.com>
2022-10-12 12:32:08 +0800

    update week4

--- week4/week4.py
+++ week4/week4.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, request, redirect, session, url_for
 app = Flask(
     __name__,
-    static_folder="templates",
+    static_folder="static",
     static_url_path="/"
 )
 
-app.secret_key = "any string but secret"
+app.secret_key = "secret"
 
 
 @app.route("/")
@@ -15,19 +15,24 @@ def home():
 
 @app.route("/signin", methods=["POST"])
 def singin():
+    # 取得 form input 的值
     account = request.form["account"]
     password = request.form["password"]
+    # 帳號&密碼接正確
     if account == "test" and password == "test":
         session["account"] = account
         return redirect("/member")
+    # 其中之一為空
     elif account == "" or password == "":
         return redirect("/error?message=請輸入帳號、密碼")
+    # 帳號&密碼都有輸入，其一有誤
     elif account != "test" or password != "test":
         return redirect("/error?message=帳號、或密碼輸入錯誤")
 
 
 @app.route("/member")
 def member():
+    # 帳號已存在，就可會員頁面，否則直接跳回首頁
     if "account" in session:
         return render_template("member.html")
     else:
@@ -42,31 +47,23 @@ def error():
 
 @app.route("/signout")
 def signout():
+    # 刪除 session 裡的帳號紀錄，並跳回首頁
     del session["account"]
     return redirect("/")
 
 
 # Optional
 
-@app.route("/square")
+@app.route("/square", methods=["POST"])
 def square():
-    # num = request.form["num"]
-    # num = int(num)
-    num = request.args.get("num", type=int, default=0)
-    squareNum = num*num
-    return render_template("square.html", squareNum=squareNum, num=num)
-
-# @app.route("/square", methods=["POST", ])
-# def square():
-#     nums = request.form["num"]
-#     return redirect(url_for("square1", num=nums))
+    nums = request.form.get("num", type=int, default=0)
+    return redirect(url_for("square1", num=nums))
 
 
-# @app.route("/square/<num>")
-# def square1(num):
-#     num = int(num)
-#     squareNum = num * num
-#     return render_template("square.html", num=num, squareNum=squareNum)
+@app.route("/square/<int:num>")
+def square1(num):
+    squareNum = num * num
+    return render_template("square.html", num=num, squareNum=squareNum)
 
 
 if __name__ == "__main__":
