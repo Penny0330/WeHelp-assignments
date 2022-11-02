a1b87d8145be5e4c276e05d818e0dde1086b9ee6
Penny Lin <ox1566@gmail.com>
2022-11-02 12:34:27 +0800

    update week7.js

--- week7/static/week7.js
+++ week7/static/week7.js
@@ -7,7 +7,9 @@ search_btn.addEventListener("click", ()=>{
         return response.json();
     })
     .then(function(success){
-        result_username.innerHTML = `${success.data.name} ( ${success.data.username} )`;
+        name = success.data.name
+        username = success.data.username
+        result_username.innerHTML = `${name} ( ${username} )`;
     })
     .catch(function(error){
         result_username.innerHTML = "無此人註冊";
